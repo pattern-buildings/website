f0a45ba8fb90f12cb7170d61301f5e7080d93dc8
Ilya Vasilyev <ilya.vasilyev@gmx.com>
2020-08-25 16:49:07 +0300

    fix downloads block on mobile

--- src/components/DownloadsBlock.vue
+++ src/components/DownloadsBlock.vue
@@ -4,11 +4,16 @@
       v-for="(item, i) in downloads"
       :key="i"
       :to="item.file || item.url || '#'"
-      class="flex items-center m-4 p-4 border-2 border-gray-300 border-dotted"
+      class="flex flex-col sm:flex-row items-center m-4 p-4 border-2 border-gray-300 border-dotted"
     >
-      <Icon :path="detectIcon(item.type)" class="w-12 h-12 text-gray-600" />
+      <Icon
+        :path="detectIcon(item.type)"
+        class="w-12 h-12 text-gray-600 flex-shrink-0"
+      />
       <span class="mx-4">{{ item.title }}</span>
-      <button class="btn-outline ml-auto text-gray-600">Download</button>
+      <button class="btn-outline sm:ml-auto my-4 text-gray-600">
+        Download
+      </button>
     </g-link>
   </div>
 </template>
